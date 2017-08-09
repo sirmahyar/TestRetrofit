d17ea71d6eaa7d1f0211e7a508d1b63f1cf43e49
sirmahyar <salamat.m76@gmail.com>
2017-08-09 11:57:32 +0430

    changes in RegisterActivity

--- app/src/main/java/mahyar/testretrofit/feature/RegisterActivity.java
+++ app/src/main/java/mahyar/testretrofit/feature/RegisterActivity.java
@@ -2,9 +2,21 @@ package mahyar.testretrofit.feature;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import mahyar.testretrofit.R;
+import mahyar.testretrofit.data.api.AuthService;
+import mahyar.testretrofit.data.models.RegisterResponseJM;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * Created by mahyar on 8/9/2017.
@@ -12,19 +24,53 @@ import butterknife.BindView;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    @BindView(mahyar.testretrofit.R.id.email_edt)
+    @BindView(R.id.email_edt)
     EditText email;
 
-    @BindView(mahyar.testretrofit.R.id.password_edt)
-    EditText pasword;
+    @BindView(R.id.password_edt)
+    EditText password;
 
-    @BindView(mahyar.testretrofit.R.id.username_edt)
+    @BindView(R.id.username_edt)
     EditText userName;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(mahyar.testretrofit.R.layout.activity_main);
-
-
+        ButterKnife.bind(this);
     }
+
+    @OnClick(R.id.register_btn)
+    void click() {
+        registerUser();
+    }
+
+    private void registerUser() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://todo.vestaak.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        AuthService service = retrofit.create(AuthService.class);
+
+        Call<RegisterResponseJM> call = service.registerUser(
+                userName.getText().toString()
+                , email.getText().toString()
+                , password.getText().toString()
+        );
+
+        call.enqueue(new Callback<RegisterResponseJM>() {
+            @Override
+            public void onResponse(Call<RegisterResponseJM> call, Response<RegisterResponseJM> response) {
+                Toast.makeText(getApplicationContext(), "done", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onFailure(Call<RegisterResponseJM> call, Throwable t) {
+                Toast.makeText(getApplicationContext(), "fail", Toast.LENGTH_LONG).show();
+                Log.d("error is ", "the error " + t.getLocalizedMessage());
+            }
+        });
+    }
+
+
 }
